5ebedd5ef34ad3ff24f1b372b846c8ebe0bb1e75
kovvik23 <08kovacs.v@gmail.com>
2024-04-15 10:43:06 +0200

    Correct gh pages base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "nds-fsd/reto-4-react-calculator-kovvik23"
+  base: 'https://nds-fsd.github.io/reto-4-react-calculator-kovvik23/'
 })
